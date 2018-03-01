68c3f43421784e8c53383a17e24d297898a00c83
Peter Vigh <vpeti88@gmail.com>
2018-03-01 17:04:51 +0100

    Refactor: Introduce TypeScript - to be finished

---
+++ scripts/main.js
@@ -0,0 +1,364 @@
+"use strict";
+var can = document.getElementById("myCan"), c = can.getContext('2d');
+/* Canvas dimensions */
+var cw = can.width, ch = can.height, Step = { ver: 0, hor: 0 };
+/* Base functions */
+function compObj(obj1, obj2) {
+    return (JSON.stringify(obj1) == JSON.stringify(obj2));
+}
+/* Board griding */
+var grid = {
+    size: 0,
+    setSize: function (s) {
+        this.size = s;
+        Step.ver = Math.trunc(cw / s);
+        Step.hor = Math.trunc(ch / s);
+    },
+    getSize: function () { return this.size; },
+    setColor: function (color) { c.strokeStyle = color; },
+    getColor: function () { return c.strokeStyle; },
+    getbgColor: function () { return can.style.backgroundColor; },
+    draw: function () {
+        var size = this.getSize();
+        c.beginPath();
+        for (var x = 0; x <= Step.ver; x++) {
+            c.moveTo(size * x, 0);
+            c.lineTo(size * x, ch);
+            for (var y = 0; y <= Step.hor; y++) {
+                c.moveTo(0, size * y);
+                c.lineTo(cw, size * y);
+            }
+        }
+        c.stroke();
+    },
+    clear: function () {
+        c.beginPath();
+        c.fillStyle = "darkgreen";
+        c.fillRect(0, 0, cw, ch);
+        c.stroke();
+    }
+};
+/* On-screen user interface */
+var hud = {
+    create: undefined,
+    lay: {
+        play: [
+            "score",
+            "lives"
+        ],
+        pause: [
+            "toast",
+            "legend"
+        ],
+        gOver: ["gameOver"]
+    },
+    getElm: function (elm) {
+        return document.getElementById(elm);
+    },
+    setText: function (elm, txt) {
+        elm.innerText = txt;
+    },
+    msg: {
+        elm: "toast",
+        addText: function (txt) { hud.getElm(this.elm).innerText = txt; },
+        show: function () { hud.show(this.elm); },
+        hide: function () { hud.hide(this.elm); }
+    },
+    score: {
+        elm: "score",
+        val: 0,
+        add: function (inc) {
+            this.val += inc;
+            this.print();
+        },
+        reset: function () {
+            this.val = 0;
+            this.print();
+        },
+        print: function () {
+            hud.getElm("score").innerText = "Score: " + this.val;
+        }
+    },
+    lives: {
+        elm: "lives",
+        val: 3,
+        dead: function () {
+            this.val -= 1;
+            this.print();
+        },
+        reset: function () {
+            this.val = 3;
+            this.print();
+        },
+        print: function () {
+            hud.getElm("lives").innerText = this.val + ' lives left';
+        }
+    },
+    show: function (elm) {
+        if (Array.isArray(elm)) {
+            for (var i = 0; i < elm.length; i++) {
+                this.getElm(elm[i]).style.display = "initial";
+            }
+        }
+        else
+            this.getElm(elm).style.display = "initial";
+    },
+    hide: function (elm) {
+        if (Array.isArray(elm)) {
+            for (var i = 0; i < elm.length; i++) {
+                this.getElm(elm[i]).style.display = "none";
+            }
+        }
+        else
+            this.getElm(elm).style.display = "none";
+    },
+};
+/* Snake */
+var snake = {
+    coords: Array(0),
+    afterCoord: { x: 0, y: 0 },
+    direction: "",
+    isGrow: false,
+    init: function () {
+        this.coords = Array(0);
+        this.coords.push({
+            x: Math.trunc(Step.ver / 2),
+            y: Math.trunc(Step.hor / 2)
+        });
+        this.draw();
+    },
+    setDir: function (dir) { this.direction = dir; },
+    getDir: function () { return this.direction; },
+    find: function (coord) {
+        return this.coords.findIndex(function (obj) {
+            return compObj(obj, coord);
+        });
+    },
+    bite: function (coord) {
+        return this.coords.slice(1).findIndex(function (obj) {
+            return (compObj(obj, coord));
+        });
+    },
+    move: function (direction) {
+        var dir = direction || this.direction;
+        var coords = this.coords;
+        var head = coords[0];
+        switch (dir) {
+            /* Y axis */
+            case "up":
+                coords.unshift({ x: head.x, y: head.y - 1 });
+                break; // Y-
+            case "down":
+                coords.unshift({ x: head.x, y: head.y + 1 });
+                break; // Y+
+            /* X axis */
+            case "left":
+                coords.unshift({ x: head.x - 1, y: head.y });
+                break; // X-
+            case "right":
+                coords.unshift({ x: head.x + 1, y: head.y });
+                break; // X+
+            default: ;
+        }
+        // Update head after step
+        head = coords[0];
+        /* Head on the food */
+        var index = food.find(head);
+        if (index > -1) {
+            food.eat(index);
+            this.isGrow = true;
+            hud.score.add(10);
+            food.place();
+        }
+        /* Assign after snake coords */
+        if (!this.isGrow)
+            this.afterCoord = coords.pop();
+        else
+            this.isGrow = false;
+        /* Bite himself */
+        if (this.bite(head) > -1) {
+            game.end();
+        }
+        /* Hit the wall */
+        if ((head.y < 0 || head.y >= Step.hor) ||
+            (head.x < 0 || head.x >= Step.ver)) {
+            dir = "";
+            game.end();
+        }
+        this.setDir(dir);
+    },
+    draw: function () {
+        var head = this.coords[0];
+        c.beginPath();
+        c.fillStyle = "#030";
+        // Draw Snake's head
+        c.fillRect(head.x * grid.getSize() + 1, head.y * grid.getSize() + 1, grid.getSize() - 2, grid.getSize() - 2);
+        // Hide Snake's track
+        if (!this.isGrow) {
+            c.fillStyle = "darkgreen";
+            c.fillRect(this.afterCoord.x * grid.getSize() + 1, this.afterCoord.y * grid.getSize() + 1, grid.getSize() - 2, grid.getSize() - 2);
+        }
+        c.stroke();
+    }
+};
+/* Food */
+var food = {
+    coords: Array(0),
+    find: function (coord) {
+        return this.coords.findIndex(function (obj) {
+            return (obj.x == coord.x && obj.y == coord.y);
+        });
+    },
+    add: function (coord) {
+        return this.coords.push(coord) - 1; // Return index of added element
+    },
+    eat: function (id) {
+        this.coords.splice(id, 1);
+    },
+    reset: function () {
+        this.coords = Array(0);
+        this.place();
+    },
+    genRandCoord: function () {
+        var rand = function (max) { return Math.floor((Math.random() * max)); };
+        return { x: rand(Step.ver), y: rand(Step.hor) };
+    },
+    place: function () {
+        var coords = this.genRandCoord(), pi = Math.PI, r = grid.getSize() / 2;
+        // Don't place food on snake
+        while (snake.find(coords) > -1) {
+            coords = this.genRandCoord();
+        }
+        // Possible to add multiple food (future feature)
+        this.add(coords);
+        c.fillStyle = "#C00";
+        c.beginPath();
+        c.arc(coords.x * grid.getSize() + r, coords.y * grid.getSize() + r, r - 1, 0, 2 * pi);
+        c.fill();
+        c.stroke();
+    }
+};
+/* Animation control */
+var anim = {
+    id: null,
+    speed: 250,
+    isPaused: true,
+    setSpeed: function (spd) { this.speed = spd; },
+    getSpeed: function () { return this.speed; },
+    play: function () {
+        if (!this.id)
+            this.id = setInterval(function () {
+                snake.move();
+                snake.draw();
+            }, this.getSpeed());
+        this.isPaused = false;
+        return;
+    },
+    pause: function () {
+        clearInterval(this.id);
+        this.id = null;
+        this.isPaused = true;
+        return;
+    },
+    toggle: function () {
+        if (this.isPaused === true)
+            this.play();
+        else
+            this.pause();
+    }
+};
+window.onkeyup = function (event) {
+    /* Cross-browser event handling */
+    var e = event || window.event;
+    var dir = snake.getDir();
+    switch (e.keyCode) {
+        /* Opposite direction test, because the snake "Never Back Down" =) */
+        case 38:
+            if (dir !== "down")
+                dir = "up";
+            break;
+        case 40:
+            if (dir !== "up")
+                dir = "down";
+            break;
+        case 37:
+            if (dir !== "right")
+                dir = "left";
+            break;
+        case 39:
+            if (dir !== "left")
+                dir = "right";
+            break;
+        case 80:
+            {
+                anim.toggle();
+                if (anim.isPaused) {
+                    hud.msg.addText("Paused");
+                    hud.msg.show();
+                }
+                else
+                    hud.msg.hide();
+            }
+            break;
+    }
+    if (36 < e.keyCode && e.keyCode < 41) {
+        if (game.getStatus() == "end")
+            game.reset();
+        else {
+            hud.msg.hide();
+            snake.setDir(dir);
+            game.play();
+        }
+    }
+};
+var game = {
+    status: "",
+    setStatus: function (st) { this.status = st; },
+    getStatus: function () { return this.status; },
+    start: function () {
+        this.setStatus("start");
+        // Grid
+        grid.setSize(25);
+        grid.setColor("#333");
+        grid.draw();
+        // Hud
+        hud.msg.addText("Press cursor keys to begin");
+        // Snake
+        snake.init();
+        // Food
+        food.place();
+    },
+    play: function () {
+        // Hud
+        hud.hide("title");
+        hud.show(hud.lay.play);
+        // Let's move
+        anim.play();
+    },
+    end: function () {
+        this.setStatus("end");
+        // Hud
+        hud.show("gameOver");
+        hud.lives.dead();
+        // Stop animation
+        anim.pause();
+    },
+    reset: function () {
+        this.setStatus("reset");
+        // Grid (clean up)
+        grid.clear();
+        grid.draw();
+        // Hud
+        hud.hide("gameOver");
+        hud.msg.addText("Press cursor keys to begin");
+        hud.msg.show();
+        if (hud.lives.val === 0) {
+            hud.lives.reset();
+            hud.score.reset();
+        }
+        // Snake
+        snake.init();
+        // Food
+        food.reset();
+    }
+};
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,49 +1,42 @@
-var can = document.getElementById("myCan"),
-    c = can.getContext('2d');
+var can = <HTMLCanvasElement>document.getElementById("myCan"),
+  c = can.getContext('2d');
 
 /* Canvas dimensions */
 var cw = can.width,
-    ch = can.height,
-    
-    Step = {ver: 0, hor: 0};
+  ch = can.height,
+  Step = { ver: 0, hor: 0 };
 
 /* Base functions */
-function compObj(obj1, obj2){ // Compare two object
+function compObj(obj1, obj2) { // Compare two object
   return (JSON.stringify(obj1) == JSON.stringify(obj2));
 }
 
 /* Board griding */
 var grid = {
   size: 0,
-  
-  setSize: function(s){
+  setSize: function(s) {
     this.size = s;
-    
-    Step.ver = parseInt(cw/s);
-    Step.hor = parseInt(ch/s);
+    Step.ver = Math.trunc(cw / s);
+    Step.hor = Math.trunc(ch / s);
   },
-  getSize: function(){return this.size;},
-  setColor: function(color){c.strokeStyle = color;},
-  getColor: function(){return c.strokeStyle;},
-  getbgColor: function(){return can.style.backgroundColor;},
-  
-  draw: function(){
+  getSize: function() { return this.size; },
+  setColor: function(color) { c.strokeStyle = color; },
+  getColor: function() { return c.strokeStyle; },
+  getbgColor: function() { return can.style.backgroundColor; },
+  draw: function() {
     var size = this.getSize();
-    
     c.beginPath();
-  
-    for(var x = 0; x <= Step.ver; x++) {
-      c.moveTo(size*x, 0);
-      c.lineTo(size*x, ch);
-    
-      for(var y = 0; y <= Step.hor; y++) {
-        c.moveTo(0, size*y);
-        c.lineTo(cw, size*y);
+    for (var x = 0; x <= Step.ver; x++) {
+      c.moveTo(size * x, 0);
+      c.lineTo(size * x, ch);
+      for (var y = 0; y <= Step.hor; y++) {
+        c.moveTo(0, size * y);
+        c.lineTo(cw, size * y);
       }
     }
     c.stroke();
   },
-  clear: function(){
+  clear: function() {
     c.beginPath();
     c.fillStyle = "darkgreen";
     c.fillRect(0, 0, cw, ch);
@@ -55,8 +48,6 @@ var grid = {
 var hud = {
   create: undefined,
   lay: { // Layouts
-    addTo: function(layName, elm){},
-    delFrom: function(layName, elm){},
     play: [
       "score",
       "lives"
@@ -67,31 +58,26 @@ var hud = {
     ],
     gOver: ["gameOver"]
   },
-  getElm: function(elm){
+  getElm: function(elm) {
     return document.getElementById(elm);
   },
-  setText: function(elm, txt){
+  setText: function(elm, txt) {
     elm.innerText = txt;
-  },
-  init: function(){
-    
   },
   msg: {
     elm: "toast",
-    addText: function(txt){hud.getElm(this.elm).innerText=txt;},
-
-    show: function(){hud.show(this.elm);},
-    hide: function(){hud.hide(this.elm);}
+    addText: function(txt) { hud.getElm(this.elm).innerText = txt; },
+    show: function() { hud.show(this.elm); },
+    hide: function() { hud.hide(this.elm); }
   },
   score: {
     elm: "score",
     val: 0,
-    
-    add: function(inc){
+    add: function(inc) {
       this.val += inc;
       this.print();
-    },    
-    reset: function(){
+    },
+    reset: function() {
       this.val = 0;
       this.print();
     },
@@ -102,12 +88,11 @@ var hud = {
   lives: {
     elm: "lives",
     val: 3,
-    
-    dead: function(){
+    dead: function() {
       this.val -= 1;
       this.print();
-    },    
-    reset: function(){
+    },
+    reset: function() {
       this.val = 3;
       this.print();
     },
@@ -115,16 +100,16 @@ var hud = {
       hud.getElm("lives").innerText = this.val + ' lives left';
     }
   },
-  show: function(elm){
+  show: function(elm) {
     if (Array.isArray(elm)) {
-      for (var i=0; i < elm.length;i++) {
+      for (var i = 0; i < elm.length; i++) {
         this.getElm(elm[i]).style.display = "initial"
       }
     } else this.getElm(elm).style.display = "initial";
   },
-  hide: function(elm){
+  hide: function(elm) {
     if (Array.isArray(elm)) {
-      for (var i=0; i < elm.length;i++) {
+      for (var i = 0; i < elm.length; i++) {
         this.getElm(elm[i]).style.display = "none"
       }
     } else this.getElm(elm).style.display = "none";
@@ -134,97 +119,96 @@ var hud = {
 /* Snake */
 var snake = {
   coords: Array(0),
-  afterCoord: {x:0, y:0},
+  afterCoord: { x: 0, y: 0 },
   direction: "",
   isGrow: false,
-  //getLen: function(){return this.coords.length;},
-  init: function(){
+  init: function() {
     this.coords = Array(0);
-    this.coords.push({x:parseInt(Step.ver/2),
-                      y:parseInt(Step.hor/2)});
+    this.coords.push({
+      x: Math.trunc(Step.ver / 2),
+      y: Math.trunc(Step.hor / 2)
+    });
     this.draw();
   },
-  setDir: function(dir){this.direction = dir;},
-  getDir: function(){return this.direction;},
-  find: function(coord){
-    return this.coords.findIndex(function(obj){
+  setDir: function(dir) { this.direction = dir; },
+  getDir: function() { return this.direction; },
+  find: function(coord) {
+    return this.coords.findIndex(function(obj) {
+      return compObj(obj, coord);
+    });
+  },
+  bite: function(coord) {
+    return this.coords.slice(1).findIndex(function (obj) {
       return (compObj(obj, coord));
     });
   },
-  bite: function(coord){
-    return this.coords.slice(1).findIndex(function(obj){
-      return (compObj(obj, coord));
-    });
-  },
-  move: function(direction){
+  move: function(direction?) {
     var dir = direction || this.direction;
     var coords = this.coords;
     var head = coords[0];
-    
-    switch(dir) {
+
+    switch (dir) {
       /* Y axis */
-      case "up":    coords.unshift({x:head.x, y:head.y-1}); break; // Y-
-      case "down":  coords.unshift({x:head.x, y:head.y+1}); break; // Y+
+      case "up": coords.unshift({ x: head.x, y: head.y - 1 }); break; // Y-
+      case "down": coords.unshift({ x: head.x, y: head.y + 1 }); break; // Y+
       /* X axis */
-      case "left":  coords.unshift({x:head.x-1, y:head.y}); break; // X-
-      case "right": coords.unshift({x:head.x+1, y:head.y}); break; // X+
+      case "left": coords.unshift({ x: head.x - 1, y: head.y }); break; // X-
+      case "right": coords.unshift({ x: head.x + 1, y: head.y }); break; // X+
       default: ;
     }
-    
+
     // Update head after step
     head = coords[0];
-    
+
     /* Head on the food */
     var index = food.find(head);
     if (index > -1) {
       food.eat(index);
-      
+
       this.isGrow = true;
-      
+
       hud.score.add(10);
-            
+
       food.place();
     }
-    
+
     /* Assign after snake coords */
     if (!this.isGrow) this.afterCoord = coords.pop();
-      else this.isGrow = false;
-    
-    /* Bite yourself */
-    if (this.bite(head)>-1) {
+    else this.isGrow = false;
+
+    /* Bite himself */
+    if (this.bite(head) > -1) {
       game.end();
     }
-    
+
     /* Hit the wall */
     if ((head.y < 0 || head.y >= Step.hor) ||
-        (head.x < 0 || head.x >= Step.ver)) {
+      (head.x < 0 || head.x >= Step.ver)) {
       dir = "";
       game.end();
     }
-    
+
     this.setDir(dir);
   },
   draw: function() {
     var head = this.coords[0];
-    
     c.beginPath();
     c.fillStyle = "#030";
 
     // Draw Snake's head
-    c.fillRect(head.x*grid.getSize()+1,
-               head.y*grid.getSize()+1,
-               grid.getSize()-2,
-               grid.getSize()-2);
+    c.fillRect(head.x * grid.getSize() + 1,
+      head.y * grid.getSize() + 1,
+      grid.getSize() - 2,
+      grid.getSize() - 2);
 
     // Hide Snake's track
     if (!this.isGrow) {
       c.fillStyle = "darkgreen";
-      c.fillRect(this.afterCoord.x*grid.getSize()+1,
-                 this.afterCoord.y*grid.getSize()+1,
-                 grid.getSize()-2,
-                 grid.getSize()-2);
+      c.fillRect(this.afterCoord.x * grid.getSize() + 1,
+        this.afterCoord.y * grid.getSize() + 1,
+        grid.getSize() - 2,
+        grid.getSize() - 2);
     }
-    
     c.stroke();
   }
 }
@@ -232,43 +216,42 @@ var snake = {
 /* Food */
 var food = {
   coords: Array(0),
-  find: function(coord){
-    return this.coords.findIndex(function(obj){
+  find: function (coord) {
+    return this.coords.findIndex(function (obj) {
       return (obj.x == coord.x && obj.y == coord.y);
     });
   },
-  add: function(coord){
-    return this.coords.push(coord)-1; // Return index of added element
+  add: function(coord) {
+    return this.coords.push(coord) - 1; // Return index of added element
   },
-  eat: function(id){
+  eat: function(id) {
     this.coords.splice(id, 1);
   },
-  reset: function(){
+  reset: function() {
     this.coords = Array(0);
     this.place();
   },
-  genRandCoord: function(){
-    var rand = function(max){return Math.floor((Math.random()*max));};
-
-    return {x:rand(Step.ver), y:rand(Step.hor)};
+  genRandCoord: function() {
+    var rand = function (max) { return Math.floor((Math.random() * max)); };
+    return { x: rand(Step.ver), y: rand(Step.hor) };
   },
-  place: function(){
+  place: function() {
     var coords = this.genRandCoord(),
-        pi = Math.PI,
-        r = grid.getSize()/2;
+      pi = Math.PI,
+      r = grid.getSize() / 2;
 
     // Don't place food on snake
-    while (snake.find(coords)>-1) {
+    while (snake.find(coords) > -1) {
       coords = this.genRandCoord();
     }
-    
+
     // Possible to add multiple food (future feature)
     this.add(coords);
 
     c.fillStyle = "#C00";
 
     c.beginPath();
-    c.arc(coords.x*grid.getSize()+r, coords.y*grid.getSize()+r, r-1, 0,2*pi);
+    c.arc(coords.x * grid.getSize() + r, coords.y * grid.getSize() + r, r - 1, 0, 2 * pi);
     c.fill();
     c.stroke();
   }
@@ -279,40 +262,39 @@ var anim = {
   id: null,
   speed: 250,
   isPaused: true,
-  
-  setSpeed: function(spd){this.speed = spd;},
-  getSpeed: function(){return this.speed;},
-  play: function(){
-    if (!this.id) this.id = setInterval(function(){ // Prevent the rerun
+  setSpeed: function(spd) { this.speed = spd; },
+  getSpeed: function() { return this.speed; },
+  play: function () {
+    if (!this.id) this.id = setInterval(function () { // Prevent the rerun
       snake.move();
       snake.draw();
     }, this.getSpeed());
     this.isPaused = false;
     return;
   },
-  pause: function(){
+  pause: function() {
     clearInterval(this.id);
     this.id = null;
     this.isPaused = true;
     return;
   },
-  toggle: function(){
+  toggle: function() {
     if (this.isPaused === true) this.play();
-      else this.pause();
+    else this.pause();
   }
 }
 
-window.onkeyup = function(ev){
+window.onkeyup = function(event: KeyboardEvent) {
   /* Cross-browser event handling */
-  var e = ev || event || window.event;
+  var e: KeyboardEvent = event || <KeyboardEvent>window.event;
   var dir = snake.getDir();
-  
-  switch(e.keyCode){
+
+  switch(e.keyCode) {
     /* Opposite direction test, because the snake "Never Back Down" =) */
-    case 38: if (dir !== "down")  dir = "up"; break;
-    case 40: if (dir !== "up")    dir = "down"; break;
+    case 38: if (dir !== "down") dir = "up"; break;
+    case 40: if (dir !== "up") dir = "down"; break;
     case 37: if (dir !== "right") dir = "left"; break;
-    case 39: if (dir !== "left")  dir = "right"; break;
+    case 39: if (dir !== "left") dir = "right"; break;
     case 80: {
       anim.toggle();
       if (anim.isPaused) {
@@ -320,11 +302,10 @@ window.onkeyup = function(ev){
         hud.msg.show();
       } else hud.msg.hide();
     }
-      break;
-    /*default: alert(e.keyCode);*/
+    break;
   }
-  
-  if (36<e.keyCode && e.keyCode<41) {
+
+  if (36 < e.keyCode && e.keyCode < 41) {
     if (game.getStatus() == "end") game.reset(); else {
       hud.msg.hide();
 
@@ -337,49 +318,49 @@ window.onkeyup = function(ev){
 
 var game = {
   status: "",
-  
-  setStatus: function(st){this.status = st;},
-  getStatus: function(){return this.status;},
-  
-  start: function(){ //Set up for first launch
+
+  setStatus: function(st) { this.status = st; },
+  getStatus: function() { return this.status; },
+
+  start: function() { //Set up for first launch
     this.setStatus("start");
     // Grid
     grid.setSize(25);
     grid.setColor("#333");
     grid.draw();
-    
+
     // Hud
     hud.msg.addText("Press cursor keys to begin");
-    
+
     // Snake
     snake.init();
-    
+
     // Food
     food.place();
   },
-  play: function(){
+  play: function() {
     // Hud
     hud.hide("title");
     hud.show(hud.lay.play);
-    
+
     // Let's move
     anim.play();
   },
-  end: function(){
+  end: function() {
     this.setStatus("end");
     // Hud
     hud.show("gameOver");
     hud.lives.dead();
-    
+
     // Stop animation
     anim.pause();
   },
-  reset: function(){
+  reset: function() {
     this.setStatus("reset");
     // Grid (clean up)
     grid.clear();
     grid.draw();
-    
+
     // Hud
     hud.hide("gameOver");
     hud.msg.addText("Press cursor keys to begin");
@@ -388,10 +369,10 @@ var game = {
       hud.lives.reset();
       hud.score.reset();
     }
-    
+
     // Snake
     snake.init();
-    
+
     // Food
     food.reset();
   }
